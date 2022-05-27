b38953d974533702cd165f5dfdceda731a1d4327
suraksha <suraksha.sharma@smarsh.com>
2022-05-27 18:19:39 +0545

    Updated main.py with testrail integration

--- main.py
+++ main.py
@@ -1,92 +1,23 @@
 import base64
 import configparser
-import json
-import requests
 from datetime import date
+from Jira import create_parent_ticket, create_subtask_ticket
+from Testrail import create_testrun, testrun_id_to_file
 
 today = date.today()
 
 config = configparser.ConfigParser()
 config.read("Config.ini")
 skype = config['SKYPE']
+user = config['USER']
 
-def basic_auth():
-    data = skype["email"] + ":" + skype["accesstoken"]
+
+def basic_auth(account):
+    data = user["email"] + ":" + account
     encodedBytes = base64.b64encode(data.encode("utf-8"))
     authCode = str(encodedBytes, "utf-8")
     return authCode
 
-
-
-def parent_payload(summary_text,description_text):
-    parent_payload = \
-        {
-            "fields":
-                {
-                    "project": {"key": "PRES"},
-                    "fixVersions": [{"name": "%s" % skype["FixVersion"]}],
-                    "summary": "%s" % summary_text,
-                    "description": "%s" % description_text,
-                    "issuetype": {"name": "Story"},
-                    "priority": {"name": "Priority 3 - Medium"},
-                    "components": [{"id": "12098"}],
-                    "assignee": {"id": "%s" % skype["Suraksha"]}
-                }
-        }
-    return parent_payload
-
-
-def subtask_payload(parent, summary_text, description_text, issuetype, priority, assignee):
-    subtask_payload = \
-        {
-            "fields":
-                {
-                    "parent":{"key": "%s" % parent},
-                    "project": {"key": "PRES"},
-                    "fixVersions": [{"name": "%s" % skype["FixVersion"]}],
-                    "summary": "%s" % summary_text,
-                    "description": "%s" % description_text,
-                    "issuetype": {"name": "%s" % issuetype},
-                    "priority": {"name": "%s" % priority},
-                    "components": [{"id": "12098"}],
-                    "assignee": {"id": "%s" % assignee }
-                }
-        }
-    return subtask_payload
-
-
-def create_parent_ticket():
-    with open('ParentTicketDescription.txt', 'r+') as f:
-        # read file
-        file_source = f.read()
-        # replace 'Skype_version' with 'relevent version' in the file
-        temp_desc = file_source.replace('version', skype["SkypeVersion"])
-    summary = "Skype 8: Compatibility test for Skype " + skype["SkypeVersion"]
-    payload = json.dumps(parent_payload(summary, temp_desc))
-    url = "https://smarsh.atlassian.net/rest/api/2/issue/"
-    authCode = basic_auth()
-    headers = {
-        'Authorization': 'Basic ' + authCode,
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(url, data=payload, headers=headers, timeout=60)
-    jsondata = response.json()
-    print('Output=', jsondata)
-    assert response.status_code == 201
-    return jsondata['key']
-
-
-def create_subtask_ticket(parent, summary, temp_desc, issuetype, priority, assignee):
-    payload = json.dumps(subtask_payload(parent, summary, temp_desc,issuetype, priority, assignee ))
-    url = "https://smarsh.atlassian.net/rest/api/2/issue/"
-    authCode = basic_auth()
-    headers = {
-        'Authorization': 'Basic ' + authCode,
-        'Content-Type': 'application/json'
-    }
-    response = requests.post(url, data=payload, headers=headers, timeout=60)
-    print('Output=', response.json())
-    assert response.status_code == 201
 
 
 # Creates a main ticket for the recently release Skype version which we have to mention in the config file
@@ -94,7 +25,7 @@ parent = create_parent_ticket()
 
 
 # create a subtask Preview Compatibility Test
-with open('Preview_Compatibility_Test_desc.txt', 'r+') as f:
+with open('./Files/Preview_Compatibility_Test_desc.txt', 'r+') as f:
     # read file
     file_source = f.read()
     # replace 'Skype_version' with 'relevent version' in the file
@@ -102,19 +33,24 @@ with open('Preview_Compatibility_Test_desc.txt', 'r+') as f:
     date = today.strftime("%B %d, %Y")
     temp_desc = temp_desc.replace('Date', date)
 summary = skype["SkypeVersion"] + " Preview Compatibility Test"
-create_subtask_ticket(parent, summary, temp_desc,"Test Sub-Task", "Priority 2 - High", skype["Suraksha"])
+Preview_ref = create_subtask_ticket(parent, summary, temp_desc,"Test Sub-Task", "Priority 2 - High", skype["Suraksha"])
+id_prev = create_testrun(summary,Preview_ref)
+
 
 # create a subtask GA Compatibility Test
-with open('GA_Compatibility_Test_desc.txt', 'r+') as f:
+with open('./Files/GA_Compatibility_Test_desc.txt', 'r+') as f:
     # read file
     file_source = f.read()
     # replace 'Skype_version' with 'relevent version' in the file
     temp_desc = file_source.replace('version', skype["SkypeVersion"])
 summary = skype["SkypeVersion"] + " GA Compatibility Test"
-create_subtask_ticket(parent, summary, temp_desc,"Test Sub-Task", "Priority 1 - Highest", skype["Suraksha"])
+GA_ref = create_subtask_ticket(parent, summary, temp_desc,"Test Sub-Task", "Priority 1 - Highest", skype["Suraksha"])
+id_ga = create_testrun(summary,GA_ref)
+
+testrun_id_to_file(id_prev,id_ga)
 
 # create a subtask Approve Skype version in Staging 2
-with open('Approve_In_Staging.txt', 'r+') as f:
+with open('./Files/Approve_In_Staging.txt', 'r+') as f:
     # read file
     file_source = f.read()
     # replace 'Skype_version' with 'relevent version' in the file
@@ -123,7 +59,7 @@ summary = "Approve Skype " + skype["SkypeVersion"] + " in Staging 2"
 create_subtask_ticket(parent, summary, temp_desc,"DB sub-task", "Priority 3 - Medium", skype["Suraksha"])
 
 # create a subtask Update Support Chart
-with open('Update_doc.txt', 'r+') as f:
+with open('./Files/Update_doc.txt', 'r+') as f:
     # read file
     file_source = f.read()
     # replace 'Skype_version' with 'relevent version' in the file
